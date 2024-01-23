595d64832dc838de563a609907e6359b7194b13f
Ricardo1817 <rmacedonio018@gamil.com>
2024-01-23 18:16:11 -0500

    to display welcome to java 5X's

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welocme to Java");
+        System.out.println("Welcome to Java");
     }
 }
